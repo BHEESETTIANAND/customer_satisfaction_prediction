13bee8d847d707e83e7810f719f5f30749da8b33
BHEESETTI ANAND <101445444+BHEESETTIANAND@users.noreply.github.com>
2024-05-07 13:47:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import joblib
 
-model=joblib.load("customer_satisfaction_prediction\best_models\RF.pkl")
+model=joblib.load("customer_satisfaction_prediction\RF.pkl")
 st.title("Customer satisfaction Prediction")
 
 st.markdown("enter the deatils mentioned below to predict whether the customer is satisfied or not")
